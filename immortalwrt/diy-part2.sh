cce543a521608974f210f3fb5f1864dbe76fe8ae
骷髅头 <lasstp5011@gmail.com>
2025-03-09 21:25:49 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.5/g' package/base-files/files/bin/config_generate
+
+# 禁用ipv6前缀
+sed -i 's/^[^#].*option ula/#&/' /etc/config/network
