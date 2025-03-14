c84650d5c6529b55d1ec1f539dc8b3d951e407b8
骷髅头 <lasstp5011@gmail.com>
2025-03-14 19:42:45 +0800

    Update diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -15,8 +15,6 @@ function merge_package(){
 mkdir package/community
 pushd package/community
 git clone --depth=1 https://github.com/fw876/helloworld
-merge_package https://github.com/DHDAXCW/lede-rockchip lede-rockchip/package/wwan/quectel_cm_5G
-merge_package https://github.com/DHDAXCW/lede-rockchip lede-rockchip/package/wwan/quectel_MHI
 popd
 
 # add luci-app-mosdns
@@ -25,6 +23,3 @@ git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/l
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-# qmodem
-git clone https://github.com/LINGJP/modem_feeds package/modem_feeds
